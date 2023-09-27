6ee340405781f6a4a1ae0adc24f47955757efe9b
Kiyarash <Kiyarashhadidian@gmail.com>
2023-09-27 09:34:43 +0330

    Add login page

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,69 @@
+export default function Page() {
+  return (
+    <div className="bg-gray-100 flex justify-center items-center h-screen max-h-screen overflow-hidden">
+      <div className="w-1/2 h-screen hidden md:block">
+        <img
+          src="https://placehold.co/800x/667fff/ffffff.png?text=Your+Image&font=Montserrat"
+          alt="Placeholder Image"
+          className="object-cover w-full h-full"
+        />
+      </div>
+      <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
+        <h1 className="text-2xl font-semibold mb-4">Login</h1>
+        <form action="#" method="POST">
+          <div className="mb-4">
+            <label htmlFor="username" className="block text-gray-600">
+              Username
+            </label>
+            <input
+              type="text"
+              id="username"
+              name="username"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              autoComplete="off"
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="block text-gray-600">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              className="w-full border border-gray-300 rounded-md py-2 px-3 focus:outline-none focus:border-blue-500"
+              autoComplete="off"
+            />
+          </div>
+          <div className="mb-4 flex items-center">
+            <input
+              type="checkbox"
+              id="remember"
+              name="remember"
+              className="text-blue-500"
+            />
+            <label htmlFor="remember" className="text-gray-600 ml-2">
+              Remember Me
+            </label>
+          </div>
+          <div className="mb-6 text-blue-500">
+            <a href="#" className="hover:underline">
+              Forgot Password?
+            </a>
+          </div>
+          <button
+            type="submit"
+            className="bg-blue-500 hover:bg-blue-600 text-white font-semibold rounded-md py-2 px-4 w-full"
+          >
+            Login
+          </button>
+        </form>
+        <div className="mt-6 text-blue-500 text-center">
+          <a href="#" className="hover:underline">
+            Sign up Here
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
