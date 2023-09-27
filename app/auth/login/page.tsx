87f208fa5bd087cabefc0ccebc96c58b70b1f92e
Kiyarash <Kiyarashhadidian@gmail.com>
2023-09-27 19:24:24 +0330

    Add input component

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -11,6 +11,7 @@ export default function Page() {
       <div className="lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
         <h1 className="text-2xl font-semibold mb-4">Login</h1>
         <form action="#" method="POST">
+          
           <div className="mb-4">
             <label htmlFor="username" className="block text-gray-600">
               Username
---
+++ components/ui/input.tsx
@@ -0,0 +1,42 @@
+type Props = {
+  label?: any;
+  inputClassName?: string;
+  containerClassName?: string;
+  type?: string;
+  placeholder?: string;
+  id: string;
+  onChange?: any;
+  defaultValue?: string;
+  value?: string | number;
+  disable?: boolean;
+};
+
+export default function Input(props: Props) {
+  const type = props.type || "text";
+  const disable = props.disable || false;
+
+  return (
+    <div className={`${props.containerClassName} space-y-2`}>
+      {props.label ? (
+        <label htmlFor={props.id} className="text-gray-600 font-semibold block">
+          {props.label}
+        </label>
+      ) : null}
+      <input
+        type={type}
+        placeholder={props.placeholder}
+        className={`${
+          props.inputClassName
+        } py-1.5 bg-sGray border border-gray-300 text-gray-500 focus:outline-none rounded-lg px-2 ${
+          props.disable ? " cursor-not-allowed" : "cursor-pointer"
+        }`}
+        id={props.id}
+        onChange={props.onChange}
+        defaultValue={props.defaultValue}
+        disabled={disable}
+        value={props.value}
+        min={0}
+      />
+    </div>
+  );
+}
